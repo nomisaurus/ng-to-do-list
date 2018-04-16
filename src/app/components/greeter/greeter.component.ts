0de9ea1bf17e4f243ec74e26ac449dd55c7be3c8
nomisaurus <mn.ulhaq@gmail.com>
2018-04-16 19:01:15 -0400

    show greeting

--- src/app/components/greeter/greeter.component.ts
+++ src/app/components/greeter/greeter.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./greeter.component.css']
 })
 export class GreeterComponent implements OnInit {
+  greeting: string;
 
   constructor() { }
 
   ngOnInit() {
+    const now = new Date();
+    const hour = now.getHours();
+    console.log(hour);
+
+    if (hour < 12) {
+      console.log('good morning');
+      this.greeting = 'Good Morning';
+    } else if (hour < 18) {
+      console.log('Good Morning');
+      this.greeting = 'Good Afternoon';
+    } else {
+      console.log('good evening');
+      this.greeting = 'Good Evening';
+
+    }
   }
 
 }
