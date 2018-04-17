44cd6d60a7afc9ab9a7baf5204e6dc5ca810acd3
nomisaurus <mn.ulhaq@gmail.com>
2018-04-17 11:58:32 -0400

    add nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { ToDoService } from './services/to-do.service';
 import { AddNewToDoComponent } from './components/add-new-to-do/add-new-to-do.component';
 import { GreeterComponent } from './components/greeter/greeter.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     AddNewToDoComponent,
-    GreeterComponent
+    GreeterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +31,9 @@ import { GreeterComponent } from './components/greeter/greeter.component';
     MatButtonModule,
     MatCheckboxModule,
     MatInputModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [ToDoService],
   bootstrap: [AppComponent]
